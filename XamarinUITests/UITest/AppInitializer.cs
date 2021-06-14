2ca5fc06e671eeab5cb5835d581c62a7f54e4d08
Matej Putnok <matejputnok@gmail.com>
2021-06-14 10:42:30 +0200

    Initial commit

---
+++ XamarinUITests/UITest/AppInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    public class AppInitializer
+    {
+
+        //com.companyname.xamarinuitests
+        //1170191635
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.xamarinuitests").StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ XamarinUITests/XamarinUITests/XamarinUITests/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinUITests
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            ClickCommand = new Command(() =>
+            {
+                Click++;
+                RaisePropertyChanged(nameof(Click));
+            });
+        }
+
+        public ICommand ClickCommand { get; set; }
+        public int Click { get; set; }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
